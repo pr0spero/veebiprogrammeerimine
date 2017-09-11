8333d2c28bcdd7e99c91366eaabdb1014b8e9673
Simmo Jaaniso <pr0spero@users.noreply.github.com>
2017-09-11 14:42:33 +0300

    Esimene PHP fail, kus mingid echod phps

--- esimene.php
+++ esimene.php
@@ -9,8 +9,14 @@
 <br>
 	<h1>h1 pealkiri</h1>
 	<p>üääöõäüöüõäööüõäöõäöüõäöüõäö</p>
-	
 
+	<?php
+		echo "<p>First php message<p>";
+		echo "<p>Today is ";
+		echo date("d.m.Y");
+		echo ".<p>";
+		echo "<p>It was " .date("H:i:s"). " when the page was opened.<p>";
+	?>
 
 </body>	
 </html>
