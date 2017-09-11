3071d5f1c22422566f6714a3ba1b9faa38979314
Simmo Jaaniso <pr0spero@users.noreply.github.com>
2017-09-11 13:48:59 +0300

    Esimene PHP fail, kus vaid HTML

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>no title</title>
+</head>
+<body>
+<br>
+	<h1>h1 pealkiri</h1>
+	<p>üääöõäüöüõäööüõäöõäöüõäöüõäö</p>
+	
+
+
+</body>	
+</html>
