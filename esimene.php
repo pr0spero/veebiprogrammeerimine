34ceb34fe8a53167972d7ca126ff691fe213bc18
Simmo Jaaniso <pr0spero@users.noreply.github.com>
2017-09-11 15:27:05 +0300

    Lisatud esimesed muutujad ja if laused ja date

--- esimene.php
+++ esimene.php
@@ -1,3 +1,42 @@
+<?php
+#muutujad
+$myName = "S";
+$myFamilyName = "J";
+
+$hourNow = date("H");
+
+#Use $hourNow value to decide what time of day it is using if statements
+
+$schoolDayStart = date("d.m.Y") ."". "8.15";
+
+$schoolBegin = strtotime($schoolDayStart);
+$timeNow = strtotime("now");
+
+echo $schoolBegin;
+
+echo $timeNow;
+
+$minutesPassed = round(($timenow - $schoolBegin) / 60);
+
+
+
+$partOfDay = "";
+if ($hourNow < 8) {
+	$partOfDay = "early morning";
+} 
+if ($hourNow >= 8 and $hourNow < 16) {
+	$partOfDay = "school hours";
+}
+if ($hourNow >= 16) {
+	$partOfDay = "free time";
+}
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 
 <html>
@@ -7,15 +46,19 @@
 </head>
 <body>
 <br>
-	<h1>h1 pealkiri</h1>
-	<p>üääöõäüöüõäööüõäöõäöüõäöüõäö</p>
+	<h1>
+	<?php
+	echo $myName . " " . $myFamilyName;
+	?>
+	</h1>
+	<!--<p>üääöõäüöüõäööüõäöõäöüõäöüõäö</p>-->
 
 	<?php
-		echo "<p>First php message<p>";
-		echo "<p>Today is ";
-		echo date("d.m.Y");
-		echo ".<p>";
-		echo "<p>It was " .date("H:i:s"). " when the page was opened.<p>";
+	echo "<p>First php message<p>";
+	echo "<p>Today is ";
+	echo date("d.m.Y") . " and it's " . $partOfDay;
+	echo ".<p>";
+	echo "<p>It was " .date("H:i:s"). " when this page was opened.<p>";
 	?>
 
 </body>	
